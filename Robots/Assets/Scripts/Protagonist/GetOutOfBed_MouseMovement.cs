5411383d4d71dc7a787423aed46587f52e39b14c
danewheaton <wheaton.dane@gmail.com>
2017-07-02 21:19:18 -0500

    animated gabe's test robot with two lines of code - this may be easier than I thought

---
+++ Robots/Assets/Scripts/Protagonist/GetOutOfBed_MouseMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetOutOfBed_MouseMovement : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        anim.speed = Mathf.Abs(Input.GetAxis("Mouse X") + Input.GetAxis("Mouse Y"));
+    }
+}
