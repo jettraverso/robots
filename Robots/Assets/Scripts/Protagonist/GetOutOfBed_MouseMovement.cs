f4861be03ebd6f11786eed7e318828ffe23fe0c7
danewheaton <wheaton.dane@gmail.com>
2017-07-09 19:43:53 -0500

    partner shifts in their sleep

--- Robots/Assets/Scripts/Protagonist/GetOutOfBed_MouseMovement.cs
+++ Robots/Assets/Scripts/Protagonist/GetOutOfBed_MouseMovement.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class GetOutOfBed_MouseMovement : MonoBehaviour
 {
-    Animator anim;
+    [SerializeField] Animator lumiAnim;
+    [SerializeField] float maxNoiseLevel = 1, movementNoise = .05f, noiseDecreaseRate = .5f;
+
+    Animator roanAnim;
+    float mouseMovement, noiseLevel;
+    int jostles;
+    bool canJostle = true;
 
     private void Start()
     {
-        anim = GetComponent<Animator>();
+        roanAnim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        anim.speed = Mathf.Abs(Input.GetAxis("Mouse X") + Input.GetAxis("Mouse Y"));
+        mouseMovement = Mathf.Abs(Input.GetAxis("Mouse X") + Input.GetAxis("Mouse Y"));
+        roanAnim.speed = mouseMovement;
+        noiseLevel = mouseMovement;
+
+        #region TODO - this noise level inication system is too bare-bones. we should replace it with one that allows the partner to react to consecutive disturbances
+        if (noiseLevel >= maxNoiseLevel)
+        {
+            print("too loud!");
+            if (canJostle)
+            {
+                jostles++;
+                canJostle = false;
+                Invoke("CoolDown", noiseDecreaseRate);
+            }
+            lumiAnim.SetInteger("Jostles", jostles);
+        }
+
+        //if (mouseMovement > 0) noiseLevel += movementNoise;
+        //if (noiseLevel > 0) noiseLevel -= Time.deltaTime * noiseDecreaseRate;
+        //else noiseLevel = 0;
+        #endregion
+
+        print(noiseLevel);
+    }
+
+    void CoolDown()
+    {
+        canJostle = true;
     }
 }
