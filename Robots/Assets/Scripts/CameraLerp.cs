3aa550614b1c4e3c317245f58b10d48f685bc1c2
danewheaton <wheaton.dane@gmail.com>
2017-07-21 16:55:23 -0500

    new dream scene

---
+++ Robots/Assets/Scripts/CameraLerp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLerp : MonoBehaviour
+{
+    [SerializeField] Transform objectToFollow;
+    [SerializeField] float lerpSpeed = 2;
+
+    private void Update()
+    {
+        Vector3 newPosition = new Vector3(objectToFollow.position.x, objectToFollow.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition, lerpSpeed * Time.deltaTime);
+    }
+}
---
+++ Robots/Assets/Scripts/DreamText_02.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DreamText_02 : MonoBehaviour
+{
+    [SerializeField] List<Transform> dreamTextNodes;
+    [SerializeField] float movementSpeed = 2, lerpSpeed = 4, range = 2,
+        mouseMovementThreshold = .5f, disappearTimer = 1, coolDownTime = 1, focalNodeZValue = 0, closestNodeProximity = .25f;
+    [SerializeField] Color outOfFocusColor = Color.gray, InFocusColor = Color.white;
+
+    Transform nodeBeingFocusedOn, nodeToDisappear;
+    Vector3 currentPosition, mousePosition;
+    float mouseMovement;
+    bool canControlMovement = true, canFadeText;
+
+    private void Start()
+    {
+        foreach (Transform t in dreamTextNodes) t.GetComponent<TextMesh>().color = outOfFocusColor;
+        nodeBeingFocusedOn = dreamTextNodes[0];
+    }
+
+    void Update()
+    {
+        UpdateMovement();
+        UpdateFocalNodeEffects();
+    }
+
+    void UpdateMovement()
+    {
+        mouseMovement = Input.GetAxis("Mouse X") + Input.GetAxis("Mouse Y");
+        mousePosition += new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+
+        if (canControlMovement)
+        {
+            transform.position = Vector3.Lerp(transform.position, mousePosition, lerpSpeed * Time.deltaTime);
+
+            if (mouseMovement > mouseMovementThreshold)
+            {
+                if (Vector3.Distance(transform.position, FindClosestNode().position) < range)
+                {
+                    canControlMovement = false;
+                }
+
+                if (canFadeText) StartCoroutine(DeactivateNode(nodeToDisappear));
+            }
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, FindClosestNode().position, lerpSpeed * Time.deltaTime);
+            print(FindClosestNode().name);
+
+            if (Vector3.Distance(transform.position, FindClosestNode().position) < closestNodeProximity)
+            {
+                nodeBeingFocusedOn = FindClosestNode().transform;
+                Invoke("CoolDown", coolDownTime);
+            }
+        }
+    }
+
+    void UpdateFocalNodeEffects()
+    {
+        // TODO make text appear sharper, clearer (remove whatever visual effects we put on it)
+
+        Vector3 newNodePosition = new Vector3(nodeBeingFocusedOn.position.x, nodeBeingFocusedOn.position.y, 0);
+        nodeBeingFocusedOn.transform.position = Vector3.Lerp(nodeBeingFocusedOn.transform.position, newNodePosition, lerpSpeed * Time.deltaTime);
+        nodeBeingFocusedOn.GetComponent<TextMesh>().color = Color.Lerp(nodeBeingFocusedOn.GetComponent<TextMesh>().color, InFocusColor, lerpSpeed * Time.deltaTime);
+    }
+
+    void CoolDown()
+    {
+        nodeToDisappear = nodeBeingFocusedOn;
+        dreamTextNodes.Remove(nodeBeingFocusedOn);
+        mousePosition = Vector3.zero;
+        canControlMovement = true;
+        canFadeText = true;
+    }
+
+    Transform FindClosestNode()
+    {
+        currentPosition = transform.position;
+        Transform closestNode = null;
+        float distanceToClosestNode = Mathf.Infinity;
+
+        for (int i = 0; i < dreamTextNodes.Count; i++)
+        {
+            Vector2 NodeDirection = dreamTextNodes[i].position - currentPosition;
+            float distanceToNode = Vector2.Distance(dreamTextNodes[i].position, currentPosition);
+
+            if (distanceToNode < distanceToClosestNode)
+            {
+                closestNode = dreamTextNodes[i];
+                distanceToClosestNode = distanceToNode;
+            }
+        }
+        if (dreamTextNodes.Count > 0) return closestNode;
+        else return transform;
+    }
+
+    IEnumerator DeactivateNode(Transform previousNode)
+    {
+        canFadeText = false;
+
+        TextMesh nodeText = previousNode.GetComponent<TextMesh>();
+        Color originalColor = nodeText.color;
+
+        float elapsedTime = 0;
+        while (elapsedTime < disappearTimer)
+        {
+            nodeText.color = Color.Lerp(originalColor, Color.clear, elapsedTime / disappearTimer);
+
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        previousNode.gameObject.SetActive(false);
+        dreamTextNodes.Remove(previousNode);
+    }
+}
